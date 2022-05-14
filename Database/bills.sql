6b09e78b3d997feeaa20aa6262f720a72c43ec31
IT20146238 <it20146238@my.sliit.lk>
2022-05-14 19:38:18 +0530

    add bill database sql file

---
+++ Database/bills.sql
@@ -0,0 +1,14 @@
+/*
+-- Query: SELECT * FROM bill.bills
+LIMIT 0, 300
+
+-- Date: 2022-05-14 19:23
+*/
+INSERT INTO `` (`billID`,`billAccNo`,`cusName`,`billUnit`,`billAmount`) VALUES (7,'B4578963214','Thirosh','115','3500');
+INSERT INTO `` (`billID`,`billAccNo`,`cusName`,`billUnit`,`billAmount`) VALUES (8,'B3698547125','Madhusha','65','1800');
+INSERT INTO `` (`billID`,`billAccNo`,`cusName`,`billUnit`,`billAmount`) VALUES (9,'B6985478521','Savi','91','2400');
+INSERT INTO `` (`billID`,`billAccNo`,`cusName`,`billUnit`,`billAmount`) VALUES (10,'B5412578965','Kasun','80','2200');
+INSERT INTO `` (`billID`,`billAccNo`,`cusName`,`billUnit`,`billAmount`) VALUES (11,'B4789652145','Nimal','101','2700');
+INSERT INTO `` (`billID`,`billAccNo`,`cusName`,`billUnit`,`billAmount`) VALUES (12,'B2547896541','Dushan','55','1400');
+INSERT INTO `` (`billID`,`billAccNo`,`cusName`,`billUnit`,`billAmount`) VALUES (13,'B5478214589','Dimalka','108','3000');
+INSERT INTO `` (`billID`,`billAccNo`,`cusName`,`billUnit`,`billAmount`) VALUES (19,'B3251452635','Sudil','220','4600');
